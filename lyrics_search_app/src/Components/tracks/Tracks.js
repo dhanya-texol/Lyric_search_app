aae17bac41a028e2a91f93cb1e07e120fa201acb
Dhanya-texol <danya@texolworld.com>
2020-07-01 16:15:09 +0530

    error solved
    console error solved

--- lyrics_search_app/src/Components/tracks/Tracks.js
+++ lyrics_search_app/src/Components/tracks/Tracks.js
@@ -18,7 +18,7 @@ class Tracks extends Component {
                                 <h3 className="text-center mb-4">{heading}</h3>
                                 <div className="row">
                                     {track_list.map(item => (
-                                        <Track key={item.track_id} track={item.track} />
+                                        <Track key={item.track.track_id} track={item.track} />
                                     ))}
                                 </div>
                             </React.Fragment>
