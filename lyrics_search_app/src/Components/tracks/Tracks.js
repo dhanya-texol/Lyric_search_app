3a55a9cae7bfff01ecb502a753e3ed871123fe5d
Dhanya-texol <danya@texolworld.com>
2020-07-01 11:11:36 +0530

    initial submit

---
+++ lyrics_search_app/src/Components/tracks/Tracks.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import {Consumer} from '../../Context'
+ class Tracks extends Component {
+    render() {
+        return (
+          <Consumer>
+{value=>{
+console.log(value);
+return<h1>haaai</h1>
+}}
+</Consumer>
+        )
+    }
+}
+export default Tracks;
